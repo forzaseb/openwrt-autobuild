cd3db72efb4b09f58abc015f53f1a537bcec9ba8
forzaseb <78002777+forzaseb@users.noreply.github.com>
2025-12-12 16:46:13 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -58,6 +58,7 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master" "pkg"
+UPDATE_PACKAGE "dns2socks-rust" "fw876/helloworld" "master" "pkg"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
